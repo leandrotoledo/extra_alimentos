e6f4a693ddc6f85c429c28b0a8176fd9e34525c3
Leandro Toledo <leandrotoledodesouza@gmail.com>
2016-03-21 22:50:22 -0300

    Adding sub-sections

--- scraper.py
+++ scraper.py
@@ -12,9 +12,21 @@ root = lxml.html.fromstring(html)
 
 
 for el in root.cssselect("div.listaDepartamentoWrapper li a"):
-    secao = el.get('href')
-    href = el.findall('span')[0].text
+    href = el.get('href')
+    secao = el.findall('span')[0].text
 
-    scraperwiki.sqlite.save(unique_keys=['secao'],
+    scraperwiki.sqlite.save(unique_keys=['secao', 'href'],
                             data={"secao": secao, "href": href},
                             table_name='secoes')
+
+for secao in scraperwiki.sql.select("* FROM secoes"):
+    html = scraperwiki.scrape(secao['href'])
+    root = lxml.html.fromstring(html)
+
+    for el in root.cssselect("div.listaDepartamentoWrapper li a"):
+        href = el.get('href')
+        subsecao = el.findall('span')[0].text
+
+        scraperwiki.sqlite.save(unique_keys=['subsecao', 'href'],
+                                data={"subsecao": subsecao, "href": href},
+                                table_name='subsecoes')
