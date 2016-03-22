9c75facf05c87d4c094d8e6f85e9fc61d762a561
Leandro Toledo <leandrotoledodesouza@gmail.com>
2016-03-21 22:43:16 -0300

    Initial commit

--- scraper.py
+++ scraper.py
@@ -1,24 +1,20 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import scraperwiki
+import lxml.html
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# Read in a page
+html = scraperwiki.scrape("http://www.deliveryextra.com.br/secoes")
+
+# Find something on the page using css selectors
+root = lxml.html.fromstring(html)
+
+
+for el in root.cssselect("div.listaDepartamentoWrapper li a"):
+    secao = el.get('href')
+    href = el.findall('span')[0].text
+
+    scraperwiki.sqlite.save(unique_keys=['secao'],
+                            data={"secao": secao, "href": href},
+                            table_name='secoes')
